d98d1026e16151754f42491f6f890d6b2bb2d7f6
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-07-05 20:29:42 -0300

    created password encryption

--- src/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,3 +1,5 @@
+import { hash } from 'bcryptjs';
+
 import { client } from "../../prisma/client";
 
 interface IUserRequest {
@@ -18,12 +20,16 @@ export class AuthenticateUserUseCase {
             throw new Error('User already exists');
         };
 
+        const passwordHash = await hash(password, 8);
+
         const user = await client.user.create({
             data: {
                 name,
-                password,
+                password: passwordHash,
                 username,
             },
         });
+
+        return user;
     };
 };
