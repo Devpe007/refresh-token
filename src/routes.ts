3682063a15dfc10ffa0f5048877120fc5d8070bd
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-07-06 14:23:33 -0300

    created AuthenticateUserController

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
 import { CreateUserController } from './useCases/createUser/CreateUserController';
+import { AuthenticateUserController } from './useCases/authenticateUser/AuthenticateUserController';
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
 const routes = Router();
 
 routes.post('/users', createUserController.hendle);
+
+routes.post('/login', authenticateUserController.handle);
 
 export { routes };
---
+++ src/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+export class AuthenticateUserController {
+    async handle(request: Request, response: Response) {
+        const { username, password } = request.body;
+
+        const authenticateUserUseCase = new AuthenticateUserUseCase();
+
+        const token = await authenticateUserUseCase.execute({
+            username,
+            password,
+        });
+
+        return response.json(token);
+    };
+};
